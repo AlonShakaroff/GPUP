600427b336cbeafeb0861e2e77d4bacb666c80af
OmerCS8 <omercs8@gmail.com>
2022-01-03 12:27:58 +0200

    Squashed commit of the following:
    commit 9e9b026af7a301c4fb7ea27363b8732a6cab9d91
    Merge: 298f507 a5f0c9c
    Author: OmerCS8 <omercs8@gmail.com>
    Date:   Mon Jan 3 12:26:06 2022 +0200
    
        Merge branch 'changesOmer' into startedAddingEngine
    
    commit 298f507eb16b0064d1ec6ef8068b971d6fd28794
    Author: AlonShakaroff <alon_lect@hotmail.com>
    Date:   Mon Jan 3 11:07:37 2022 +0200
    
        engine integration

---
+++ SystemEngine/src/task/SimulationTask.java
@@ -0,0 +1,57 @@
+package task;
+
+import target.Target;
+import userinterface.Communicator;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
+
+public class SimulationTask extends Task{
+    private final int processTimeInMS;
+    private final boolean isRandom;
+    private final float successChance;
+    private final float warningChance;
+    private final Random random;
+
+    public SimulationTask(int processTimeInMS,boolean isRandom,float successChance,float warningChance, Communicator communicator){
+        super(communicator,"Simulation task" );
+        this.isRandom  = isRandom;
+        this.processTimeInMS = processTimeInMS;
+        this.successChance = successChance;
+        this.warningChance = warningChance;
+        random = new Random();
+    }
+    @Override
+    public void runTaskOnTarget(Target target) {
+        int runTime;
+        float randSuccess = random.nextFloat();
+        float randWarning = random.nextFloat();
+        if (isRandom)
+            runTime = random.nextInt(processTimeInMS);
+        else
+            runTime = processTimeInMS;
+
+        target.setTargetTaskBegin(Instant.now());
+        communicator.printTaskIsStarting(this,target);
+        fileSaver.printTaskIsStarting(target.getName(),getTaskName());
+
+        try { Thread.sleep(runTime);
+        } catch (InterruptedException ignored) { }
+        target.setTargetTaskEnd(Instant.now());
+
+        communicator.printTaskIsEnding(this,target ,runTime*0.001f);
+        fileSaver.printTaskIsEnding(target.getName(),getTaskName() ,runTime*0.001f);
+
+        if (randSuccess > successChance){
+            target.setResult(Target.Result.FAILURE);
+        }
+        else if (randWarning < warningChance)
+            target.setResult(Target.Result.WARNING);
+        else
+            target.setResult(Target.Result.SUCCESS);
+
+        target.setTargetTaskTime(Duration.between(target.getTargetTaskBegin(),
+                target.getTargetTaskEnd()));
+    }
+}
